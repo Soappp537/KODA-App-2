d653bcdc17b62b4615cc259bab3bd75b416a8170
Soapp <80211330+Soappp537@users.noreply.github.com>
2024-05-12 13:40:38 +0800

    Update MyDeviceAdminReceiver.kt

--- app/src/main/java/com/example/kodaapplication/Receiver/MyDeviceAdminReceiver.kt
+++ app/src/main/java/com/example/kodaapplication/Receiver/MyDeviceAdminReceiver.kt
@@ -16,8 +16,19 @@ class MyDeviceAdminReceiver : DeviceAdminReceiver() {
         showToast(context, "Device admin disabled")
     }
 
+    /*mga bago:*/
+    override fun onLockTaskModeEntering(context: Context, intent: Intent, pkg: String) {
+        super.onLockTaskModeEntering(context, intent, pkg)
+        showToast(context, "Lock task mode entered")
+    }
+
+    override fun onLockTaskModeExiting(context: Context, intent: Intent) {
+        super.onLockTaskModeExiting(context, intent)
+        showToast(context, "Lock task mode exited")
+    }
+    /*mga bago:*/
+
     private fun showToast(context: Context, message: String) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
 }
-
